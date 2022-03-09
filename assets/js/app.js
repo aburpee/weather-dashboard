c32c8da3d6b7285e254e832b13c730f659e83ab6
bonobo <arburpee@gmail.com>
2022-03-08 18:15:53 -0800

    added future weather forecasting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 const apiKey = '69a46a10fbf2456deef572e3a4f08645'
 let searchFormEl = document.querySelector('#search-form');
 let locationInputEl = document.querySelector('#location');
+let cardContainerEl = document.querySelector('#weatherCard')
+let todayContainerEl = document.querySelector('#todayContainer')
 
 let formSubmitHandler = function(event) {
     event.preventDefault();
@@ -14,7 +16,7 @@ let formSubmitHandler = function(event) {
 }
 
 let getDailyWeather = function(city) {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
@@ -30,6 +32,43 @@ let getDailyWeather = function(city) {
 let displayWeather = function(weather, searchTerm) {
     console.log(weather);
     console.log(searchTerm);
+    let today = new Date();
+    let dd = String(today.getDate());
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy
+    
+    for (var i = 1; i < 6; i++) {
+        let dailyWeather = weather.list[i].main.temp
+        let dailyWind = weather.list[i].wind.speed
+        let dailyVisibility = weather.list[i].weather[0].description
+        let dailyVisibilityIcon = weather.list[i].weather[0].icon
+        
+        let weatherEl = document.createElement('div');
+        weatherEl.classList = 'card col-sm-2 day1'
+
+        let titleEl = document.createElement('h5');
+        titleEl.textContent = today
+        
+        let tempEl = document.createElement('span')
+        tempEl.textContent = "Temp: " + dailyWeather;
+        
+        let windEl = document.createElement('span')
+        windEl.textContent = "Wind Speed: " + dailyWind
+
+        let visEl = document.createElement('span')
+        visEl.textContent = "Visibility: " + dailyVisibility + ' ' + dailyVisibilityIcon
+        
+        weatherEl.appendChild(titleEl)
+        weatherEl.appendChild(tempEl);
+        weatherEl.appendChild(windEl);
+        weatherEl.appendChild(visEl);
+        cardContainerEl.appendChild(weatherEl);
+
+        dd++
+        today = mm + '/' + dd + '/' + yyyy
+    }
+
 }
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
