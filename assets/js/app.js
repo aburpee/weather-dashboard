560cc4d285d9221dfd6df6a399eda4446c4dc5d0
bonobo <arburpee@gmail.com>
2022-03-09 20:10:01 -0800

    added icons and today's weather

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,6 +31,36 @@ let getDailyWeather = function(city) {
     })
 };
 
+let oneCallApi = function(lat, long) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude={part}&appid=${apiKey}`
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                let backgroundColor
+                if (data.current.uvi > 7) {
+                    backgroundColor = 'bg-danger'
+                } else if (data.current.uvi >2.9) {
+                    backgroundColor = 'bg-warning'
+                } else {
+                    backgroundColor = 'bg-success'
+                }
+                document.getElementById('todayContainer').innerHTML=
+                `<div class="container">
+                <h6>City:${data}</h6>
+                <img src = 'https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png'>
+                <p>Temp: ${data.current.temp} F</p>
+                <p>Wind: ${data.current.wind_speed} mph</p>
+                <p>Humidity: ${data.current.humidity} %</p>
+                <p class = '${backgroundColor}'>UV Index: ${data.current.uvi}</p>
+                `
+            })
+        } else {
+            alert('error: weather not found')
+        }
+    })
+}
+
 let displayWeather = function(weather, searchTerm) {
     
     console.log(weather);
@@ -41,21 +71,21 @@ let displayWeather = function(weather, searchTerm) {
     let mm = String(today.getMonth() + 1).padStart(2, '0');
     var yyyy = today.getFullYear();
     today = mm + '/' + dd + '/' + yyyy
-    
-    for (var i = 1; i < 6; i++) {
+    let lat = weather.city.coord.lat
+    let long = weather.city.coord.lon
+
+    oneCallApi(lat, long)
+
+    for (var i = 0; i < weather.list.length; i+=8) {
         let dailyWeather = weather.list[i].main.temp
         let dailyWind = weather.list[i].wind.speed
         let dailyHumidity = weather.list[i].main.humidity
-        let dailyIcon = weather.list[i].weather[0].icon
         
         let weatherEl = document.createElement('div');
         weatherEl.classList = 'card col-sm-2 day1'
 
         let titleEl = document.createElement('h5');
         titleEl.textContent = today
-
-        let iconEl = document.createElement('i');
-        iconEl.setAttribute('class', dailyIcon)
         
         let tempEl = document.createElement('span')
         tempEl.textContent = "Temp: " + dailyWeather + ' F'
@@ -67,9 +97,11 @@ let displayWeather = function(weather, searchTerm) {
         // visEl.innerHTML = `<i class = 'dailyVisibilityIcon'></i>`
         humidEl.textContent = "Humidity: " + dailyHumidity + ' %'
         
+        let weatherIcon = document.createElement('img')
+        weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${weather.list[i].weather[0].icon}@2x.png`)
         
         weatherEl.appendChild(titleEl);
-        weatherEl.appendChild(iconEl);
+        weatherEl.appendChild(weatherIcon);
         weatherEl.appendChild(tempEl);
         weatherEl.appendChild(windEl);
         weatherEl.appendChild(humidEl);
